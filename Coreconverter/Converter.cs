c764791eec0eff4a3cda6ad402fea49edc57446b
nemishnk <nemish452@gmail.com>
2019-06-27 17:17:26 +0530

    Initial Commit

---
+++ Coreconverter/Converter.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Coreconverter
+{
+    public class Converter
+    {
+        public DataTable GetDataTableFromJsonString(string json)
+        {
+            var jsonLinq = JObject.Parse(json);
+
+            // Find the first array using Linq  
+            var srcArray = jsonLinq.Descendants().Where(d => d is JArray).First();
+            var trgArray = new JArray();
+            foreach (JObject row in srcArray.Children<JObject>())
+            {
+                var cleanRow = new JObject();
+                foreach (JProperty column in row.Properties())
+                {
+                    // Only include JValue types  
+                    if (column.Value is JValue)
+                    {
+                        cleanRow.Add(column.Name, column.Value);
+                    }
+                }
+                trgArray.Add(cleanRow);
+            }
+
+            return JsonConvert.DeserializeObject<DataTable>(trgArray.ToString());
+        }
+
+        public void CreateCSVFile(DataTable dt, string strFilePath)
+        {
+            #region Export Grid to CSV
+            // Create the CSV file to which grid data will be exported.
+            StreamWriter sw = new StreamWriter(strFilePath, false);
+            // First we will write the headers.
+            //DataTable dt = m_dsProducts.Tables[0];
+            int iColCount = dt.Columns.Count;
+
+            for (int i = 0; i < iColCount; i++)
+            {
+                sw.Write(dt.Columns[i]);
+                if (i < iColCount - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+            // Now write all the rows.
+            foreach (DataRow dr in dt.Rows)
+            {
+                for (int i = 0; i < iColCount; i++)
+                {
+                    if (!Convert.IsDBNull(dr[i]))
+                    {
+                        sw.Write(dr[i].ToString());
+                    }
+                    if (i < iColCount - 1)
+                    {
+                        sw.Write(",");
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
+
+            #endregion
+
+        }
+
+        public void CreateXMLFromJSON(String JSON, String FilePath)
+        {
+            XmlDocument doc =(XmlDocument)JsonConvert.DeserializeXmlNode(JSON,"Root");
+            XNode node = JsonConvert.DeserializeXNode(JSON, "Root");
+            //Save the document to a file.  
+            doc.Save(FilePath);
+        }
+    }
+
+}
---
+++ UI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Coreconverter;
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Json ="{'Result':[{'IsEnabled': true,'Id': 10015,'Name': 'Reena'},{'IsEnabled': true,'Id': 10016,'Name': 'Anil Singh'},{'IsEnabled': false,'Id': 10017,'Name': 'Aradhya','__metadata': { 'Id': 14013 }}]}";
+            string FilePath = "H:\\Vis 17\\Testfile";
+            Converter converter = new Converter();
+            DataTable dataTable = new DataTable();
+           dataTable=converter.GetDataTableFromJsonString(Json);
+            //converter.CreateCSVFile(dataTable,FilePath);
+            converter.CreateXMLFromJSON(Json, FilePath);
+        }
+
+    }
+}
